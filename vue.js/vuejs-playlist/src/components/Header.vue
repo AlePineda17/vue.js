ed3303380f3b5250ef1bd962ae5e2a8455e62a2e
AlePineda17 <alexis.pineda.menendez@gmail.com>
2019-09-12 15:18:57 -0600

    changes

---
+++ vue.js/vuejs-playlist/src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+  <header>
+    <h1 v-on:click="changeTitle">{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+import { bus } from '../main';
+
+export default {
+  props: {
+      title: {
+        type: String,
+        required: true
+      }
+    },
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+      changeTitle: function(){
+        //this.$emit('changeTitle', 'Vue Ninjas');
+        this.title = 'Vue Ninjas';
+        bus.$emit('titleChanged', 'Vue Ninjas');
+      }
+    },
+    //lifecycle hooks
+    beforeCreate(){
+      alert('beforeCreate');
+    },
+    created(){
+      alert('created');
+    },
+    beforeMount(){
+      alert('mounted');
+    },
+    mounted(){
+      alert('mounted');
+    },
+    beforeUpdate(){
+      alert('beforeUpdated');
+    },
+    updated(){
+      alert('updated');
+    }
+}
+</script>
+
+<style scoped>
+header{
+  background: lightgreen;
+  padding: 10px;
+}
+
+h1{
+  color: #222;
+  text-align: center;
+}
+</style>
